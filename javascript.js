7a64de2e2565fd77052d983599f56d97a10b467d
lhiga <lhiga@dc.uba.ar>
2023-11-19 16:38:03 -0300

    Add mouseover to paint div

--- javascript.js
+++ javascript.js
@@ -7,8 +7,16 @@ for (let i = 0; i < 16; i ++) {
     row.className = "row";
     for (let j = 0; j < 16; j++) {
         const square = document.createElement("div");
-        square.className = "square"
+        square.className = "square";
         row.appendChild(square);
     }
     mainDiv.appendChild(row);
 }
+
+const squareDivs = document.querySelectorAll(".square");
+
+squareDivs.forEach(function(item) {
+    item.addEventListener("mouseover", function() {
+        item.className = "square-filled";
+    });
+});
