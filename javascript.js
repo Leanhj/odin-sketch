83d0f0c28e4a2a3ceb71ea69b5fbf6727e633b0f
lhiga <lhiga@dc.uba.ar>
2023-11-19 17:15:04 -0300

    Add createGrid function

--- javascript.js
+++ javascript.js
@@ -1,22 +1,36 @@
+const CANVAS_SIZE = 512;
+let userSize = 0;
+
 const body = document.querySelector("body");
 const mainDiv = document.createElement("div");
 body.appendChild(mainDiv);
 
-for (let i = 0; i < 16; i ++) {
-    const row = document.createElement("div");
-    row.className = "row";
-    for (let j = 0; j < 16; j++) {
-        const square = document.createElement("div");
-        square.className = "square";
-        row.appendChild(square);
+function createGrid(size) {
+    userSize = size;
+    for (let i = 0; i < size; i ++) {
+        const row = document.createElement("div");
+        row.className = "row";
+        for (let j = 0; j < size; j++) {
+            const square = document.createElement("div");
+            square.className = "square";
+            square.style.height = CANVAS_SIZE/userSize + "px";
+            square.style.width = CANVAS_SIZE/userSize + "px";
+            row.appendChild(square);
+        }
+        mainDiv.appendChild(row);
     }
-    mainDiv.appendChild(row);
 }
+
+createGrid(16);
+
+mainDiv.className = "canvas-container";
 
 const squareDivs = document.querySelectorAll(".square");
 
 squareDivs.forEach(function(item) {
     item.addEventListener("mouseover", function() {
         item.className = "square-filled";
+        item.style.height = CANVAS_SIZE/userSize + "px";
+        item.style.width = CANVAS_SIZE/userSize + "px";
     });
 });
